7e188b98ad4d689aeaa9f67da5ee876958ee5d3d
Alan Lino <alanlino@gmail.com>
2024-09-10 17:00:30 -0300

    strproducer update

--- strproducer/src/main/java/com/example/strproducer/services/StringProducerService.java
+++ strproducer/src/main/java/com/example/strproducer/services/StringProducerService.java
@@ -1,11 +1,15 @@
 package com.example.strproducer.services;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.function.BiConsumer;
 
-
+@Log4j2
 @RequiredArgsConstructor
 @Service
 public class StringProducerService {
@@ -13,6 +17,15 @@ public class StringProducerService {
     private final KafkaTemplate<String, String> kafkaTemplate;
 
     public void sendMessage(String message) {
-        kafkaTemplate.send("str-topic", message);
+
+        kafkaTemplate.send("str-topic", message).whenComplete((result, e) -> {
+            if (e != null) {
+                log.error("Mensagem não enviada: {}", e.getMessage());
+            } else {
+                log.info("Mensagem enviada: {}", message);
+                log.info("Partição: {}",result.getRecordMetadata().partition());
+            }
+        });
+
     }
 }
